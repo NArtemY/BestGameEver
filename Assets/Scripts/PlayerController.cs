7c5c00ce43eb5c3671e5f113bbdaabec012bdabe
Artem <a.yu.novoselov@yandex.ru>
2019-12-06 22:09:07 +0300

    Первый не удался))

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed * 1.5f);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Cube")
+        {
+            Destroy (other.gameObject);
+        }
+    }
+}
